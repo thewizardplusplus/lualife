5e9ee3b57dbc3c7da11e79c0022cab0494244eab
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-24 23:00:00 +0300

    Add tests for the populate() function

--- life_test.lua
+++ life_test.lua
@@ -23,4 +23,50 @@ function TestNeighbors:test()
   luaunit.assertEquals(neighbors, 3)
 end
 
+TestPopulate = {}
+
+function TestPopulate:testBlinker()
+  local field = Field:new(Size:new(3, 3))
+  field:set(Point:new(0, 1))
+  field:set(Point:new(1, 1))
+  field:set(Point:new(2, 1))
+
+  local nextField = life.populate(field)
+
+  local wantNextField =
+    Field:new(Size:new(3, 3))
+  wantNextField:set(Point:new(1, 0))
+  wantNextField:set(Point:new(1, 1))
+  wantNextField:set(Point:new(1, 2))
+
+  luaunit.assertEquals(
+    nextField,
+    wantNextField
+  )
+end
+
+function TestPopulate:testGlider()
+  local field = Field:new(Size:new(4, 4))
+  field:set(Point:new(1, 0))
+  field:set(Point:new(2, 1))
+  field:set(Point:new(0, 2))
+  field:set(Point:new(1, 2))
+  field:set(Point:new(2, 2))
+
+  local nextField = life.populate(field)
+
+  local wantNextField =
+    Field:new(Size:new(4, 4))
+  wantNextField:set(Point:new(0, 1))
+  wantNextField:set(Point:new(2, 1))
+  wantNextField:set(Point:new(1, 2))
+  wantNextField:set(Point:new(2, 2))
+  wantNextField:set(Point:new(1, 3))
+
+  luaunit.assertEquals(
+    nextField,
+    wantNextField
+  )
+end
+
 os.exit(luaunit.run()) 
