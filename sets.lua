8eb49cb08fd080b7fcb927e9dbb9f03f80d81851
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-07 23:00:00 +0300

    Add the sets.complement() method

--- sets.lua
+++ sets.lua
@@ -27,4 +27,25 @@ function sets.union(base, additional, offset)
   end)
 end
 
+---
+-- @tparam Field base
+-- @tparam Field additional
+-- @tparam Point offset
+-- @treturn Field
+function sets.complement(base, additional, offset)
+  assert(base:isInstanceOf(Field))
+  assert(additional:isInstanceOf(Field))
+  assert(offset:isInstanceOf(Point))
+
+  local inverted_offset = offset:scale(-1)
+  return base:map(function(point, contains)
+    if not contains then
+      return false
+    end
+
+    local shifted_point = point:translate(inverted_offset)
+    return not additional:contains(shifted_point)
+  end)
+end
+
 return sets
--- sets_test.lua
+++ sets_test.lua
@@ -48,3 +48,21 @@ function TestSets.test_union_out_of_size()
   luaunit.assert_true(unioned_field:isInstanceOf(Field))
   luaunit.assert_equals(unioned_field, want_unioned_field)
 end
+
+function TestSets.test_complement()
+  local field = Field:new(Size:new(3, 3))
+  field:set(Point:new(0, 0))
+  field:set(Point:new(1, 0))
+  field:set(Point:new(0, 1))
+  field:set(Point:new(1, 1))
+
+  local complemented_field = sets.complement(field, field, Point:new(1, 1))
+
+  local want_complemented_field = Field:new(Size:new(3, 3))
+  want_complemented_field:set(Point:new(0, 0))
+  want_complemented_field:set(Point:new(1, 0))
+  want_complemented_field:set(Point:new(0, 1))
+
+  luaunit.assert_true(complemented_field:isInstanceOf(Field))
+  luaunit.assert_equals(complemented_field, want_complemented_field)
+end
