eb845f3ed19daeb0812d6351bf750fae9df7469f
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-13 23:00:00 +0300

    Implement the matrix.rotate() function

--- matrix.lua
+++ matrix.lua
@@ -15,7 +15,31 @@ function matrix.rotate(field)
     error("rotation of the non-square matrix")
   end
 
-  return field
+  local rotated_field = Field:new(field.size)
+  local last_index = field.size.width - 1
+  for x = 0, field.size.width / 2 - 1 do
+    for y = x, last_index - x - 1 do
+      local top_left = Point:new(x, y)
+      local top_right = Point:new(last_index - y, x)
+      local bottom_left = Point:new(y, last_index - x)
+      local bottom_right = Point:new(last_index - x, last_index - y)
+
+      if field:contains(bottom_left) then
+        rotated_field:set(top_left)
+      end
+      if field:contains(bottom_right) then
+        rotated_field:set(bottom_left)
+      end
+      if field:contains(top_right) then
+        rotated_field:set(bottom_right)
+      end
+      if field:contains(top_left) then
+        rotated_field:set(top_right)
+      end
+    end
+  end
+
+  return rotated_field
 end
 
 return matrix
