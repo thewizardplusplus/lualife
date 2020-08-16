60e8cc968be84e9cac95c2d476d3208786f12563
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-16 23:00:00 +0300

    Fix the sets.union() function

--- sets.lua
+++ sets.lua
@@ -9,21 +9,13 @@ local sets = {}
 ---
 -- @tparam Field base
 -- @tparam Field additional
--- @tparam Point offset
 -- @treturn Field
-function sets.union(base, additional, offset)
+function sets.union(base, additional)
   assert(base:isInstanceOf(Field))
   assert(additional:isInstanceOf(Field))
-  assert(offset:isInstanceOf(Point))
 
-  local inverted_offset = offset:scale(-1)
   return base:map(function(point, contains)
-    if contains then
-      return true
-    end
-
-    local shifted_point = point:translate(inverted_offset)
-    return additional:contains(shifted_point)
+    return contains or additional:contains(point)
   end)
 end
 
