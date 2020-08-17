6ca6d994718036827c9f55cbcd7cdb77c09957ee
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-17 23:00:00 +0300

    Extend tests of the matrix.rotate() function

--- matrix_test.lua
+++ matrix_test.lua
@@ -2,6 +2,7 @@ local luaunit = require("luaunit")
 local Size = require("lualife.models.size")
 local Point = require("lualife.models.point")
 local Field = require("lualife.models.field")
+local PlacedField = require("lualife.models.placed_field")
 local matrix = require("lualife.matrix")
 
 -- luacheck: globals TestMatrix
@@ -67,6 +68,60 @@ function TestMatrix.test_rotate_even()
   want_rotated_field:set(Point:new(2, 2))
   want_rotated_field:set(Point:new(3, 2))
   want_rotated_field:set(Point:new(3, 3))
+
+  luaunit.assert_true(rotated_field:isInstanceOf(Field))
+  luaunit.assert_equals(rotated_field, want_rotated_field)
+end
+
+function TestMatrix.test_rotate_placed_odd()
+  local field = PlacedField:new(Size:new(3, 3), Point:new(23, 42))
+  field:set(Point:new(23, 42))
+  field:set(Point:new(24, 42))
+  field:set(Point:new(25, 42))
+  field:set(Point:new(23, 43))
+  field:set(Point:new(24, 43))
+  field:set(Point:new(23, 44))
+
+  local rotated_field = matrix.rotate(field)
+
+  local want_rotated_field = PlacedField:new(Size:new(3, 3), Point:new(23, 42))
+  want_rotated_field:set(Point:new(23, 42))
+  want_rotated_field:set(Point:new(24, 42))
+  want_rotated_field:set(Point:new(25, 42))
+  want_rotated_field:set(Point:new(24, 43))
+  want_rotated_field:set(Point:new(25, 43))
+  want_rotated_field:set(Point:new(25, 44))
+
+  luaunit.assert_true(rotated_field:isInstanceOf(Field))
+  luaunit.assert_equals(rotated_field, want_rotated_field)
+end
+
+function TestMatrix.test_rotate_placed_even()
+  local field = PlacedField:new(Size:new(4, 4), Point:new(23, 42))
+  field:set(Point:new(23, 42))
+  field:set(Point:new(24, 42))
+  field:set(Point:new(25, 42))
+  field:set(Point:new(26, 42))
+  field:set(Point:new(23, 43))
+  field:set(Point:new(24, 43))
+  field:set(Point:new(25, 43))
+  field:set(Point:new(23, 44))
+  field:set(Point:new(24, 44))
+  field:set(Point:new(23, 45))
+
+  local rotated_field = matrix.rotate(field)
+
+  local want_rotated_field = PlacedField:new(Size:new(4, 4), Point:new(23, 42))
+  want_rotated_field:set(Point:new(23, 42))
+  want_rotated_field:set(Point:new(24, 42))
+  want_rotated_field:set(Point:new(25, 42))
+  want_rotated_field:set(Point:new(26, 42))
+  want_rotated_field:set(Point:new(24, 43))
+  want_rotated_field:set(Point:new(25, 43))
+  want_rotated_field:set(Point:new(26, 43))
+  want_rotated_field:set(Point:new(25, 44))
+  want_rotated_field:set(Point:new(26, 44))
+  want_rotated_field:set(Point:new(26, 45))
 
   luaunit.assert_true(rotated_field:isInstanceOf(Field))
   luaunit.assert_equals(rotated_field, want_rotated_field)
