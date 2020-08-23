67916744103600998b5044324ac50c1f1a2bbba0
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-23 23:00:00 +0300

    Extend tests of the random.generate_with_limits() function

--- random_test.lua
+++ random_test.lua
@@ -163,3 +163,23 @@ function TestRandom.test_generate_with_limits_placed()
     ["{x = 2,y = 2}"] = true,
   })
 end
+
+function TestRandom.test_generate_with_limits_partial()
+  math.randomseed(1)
+
+  local sample = Field:new(Size:new(3, 3))
+  local field = random.generate_with_limits(sample)
+
+  luaunit.assert_true(field.isInstanceOf and field:isInstanceOf(Field))
+
+  luaunit.assert_true(field.size:isInstanceOf(Size))
+  luaunit.assert_is(field.size, sample.size)
+
+  luaunit.assert_is_table(field._cells)
+  luaunit.assert_equals(field._cells, {
+    ["{x = 0,y = 0}"] = true,
+    ["{x = 1,y = 1}"] = true,
+    ["{x = 1,y = 2}"] = true,
+    ["{x = 2,y = 1}"] = true,
+  })
+end
