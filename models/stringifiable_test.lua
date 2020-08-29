60beb91cf92901272838ec5d03ac1cb61460ffd0
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-08-29 23:00:00 +0300

    Fix tests of the Stringifiable mixin

--- models/stringifiable_test.lua
+++ models/stringifiable_test.lua
@@ -1,11 +1,21 @@
 local luaunit = require("luaunit")
-local MockClass = require("lualife.models.mockclass")
+local middleclass = require("middleclass")
+local Stringifiable = require("lualife.models.stringifiable")
 
 -- luacheck: globals TestStringifiable
 TestStringifiable = {}
 
 function TestStringifiable.test_tostring()
-  local mock = MockClass:new(2.3, "test")
+  local MockClass = middleclass("MockClass")
+  MockClass:include(Stringifiable)
+  MockClass.__data = function()
+    return {
+      number = 2.3,
+      text = "test",
+    }
+  end
+
+  local mock = MockClass:new()
   local text = tostring(mock)
 
   luaunit.assert_is_string(text)
