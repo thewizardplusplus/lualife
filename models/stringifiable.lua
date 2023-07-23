c3bbaea505834829cd32e230c455fdbaf9ef485a
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-07-23 21:27:58 +0600

    Use the `luatypechecks` package in the `Stringifiable` mixin

--- models/stringifiable.lua
+++ models/stringifiable.lua
@@ -2,14 +2,14 @@
 -- @classmod Stringifiable
 
 local inspect = require("inspect")
-local types = require("lualife.types")
+local assertions = require("luatypechecks.assertions")
 
 local Stringifiable = {}
 
 ---
 -- @treturn string stringified result of the __data() metamethod
 function Stringifiable:__tostring()
-  assert(types.has_metamethod(self, "__data"))
+  assertions.has_metamethods(self, {"__data"})
 
   return inspect(self:__data(), {
     indent = "",
