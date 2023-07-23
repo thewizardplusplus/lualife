192b37260870a0dec8c3286ffdf92c53867d7b03
thewizardplusplus <thewizardplusplus@yandex.ru>
2023-07-23 21:23:12 +0600

    Use the `luatypechecks` package in the `Point` class

--- models/point_test.lua
+++ models/point_test.lua
@@ -1,5 +1,5 @@
 local luaunit = require("luaunit")
-local types = require("lualife.types")
+local checks = require("luatypechecks.checks")
 local Point = require("lualife.models.point")
 
 -- luacheck: globals TestPoint
@@ -8,7 +8,7 @@ TestPoint = {}
 function TestPoint.test_new()
   local point = Point:new(23, 42)
 
-  luaunit.assert_true(types.is_instance(point, Point))
+  luaunit.assert_true(checks.is_instance(point, Point))
 
   luaunit.assert_is_number(point.x)
   luaunit.assert_equals(point.x, 23)
@@ -29,7 +29,7 @@ function TestPoint.test_translate()
   local point = Point:new(5, 12)
   local translated_point = point:translate(Point:new(23, 42))
 
-  luaunit.assert_true(types.is_instance(translated_point, Point))
+  luaunit.assert_true(checks.is_instance(translated_point, Point))
   luaunit.assert_equals(translated_point, Point:new(28, 54))
 end
 
@@ -37,6 +37,6 @@ function TestPoint.test_scale()
   local point = Point:new(5, 12)
   local scaled_point = point:scale(23)
 
-  luaunit.assert_true(types.is_instance(scaled_point, Point))
+  luaunit.assert_true(checks.is_instance(scaled_point, Point))
   luaunit.assert_equals(scaled_point, Point:new(115, 276))
 end
