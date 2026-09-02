30f02e8a7ac9b85ade054737a9349b9ff12d964b
thewizardplusplus <thewizardplusplus@yandex.ru>
2026-09-03 04:51:01 +0500

    Use the `luamath` package in the `life` package

--- life.lua
+++ life.lua
@@ -2,8 +2,11 @@
 -- @module life
 
 local assertions = require("luatypechecks.assertions")
-local Point = require("lualife.models.point")
+local Vector2D = require("luamath.vector2d")
+local Range = require("luamath.models.range")
 local Field = require("lualife.models.field")
+
+local _NEIGHBOR_OFFSET_RANGE = Range:new(-1, 1)
 
 local life = {}
 
@@ -14,7 +17,7 @@ function life.populate(field)
   assertions.is_instance(field, Field)
 
   return field:map(function(point, contains)
-    assertions.is_instance(point, Point)
+    assertions.is_instance(point, Vector2D)
     assertions.is_boolean(contains)
 
     local neighbors = life._neighbors(field, point)
@@ -24,16 +27,16 @@ end
 
 ---
 -- @tparam Field field
--- @tparam Point point
+-- @tparam Vector2D point
 -- @treturn int [0, 8]
 function life._neighbors(field, point)
   assertions.is_instance(field, Field)
-  assertions.is_instance(point, Point)
+  assertions.is_instance(point, Vector2D)
 
   local neighbors = 0
-  for dy = -1, 1 do
-    for dx = -1, 1 do
-      local translated_point = point:translate(Point:new(dx, dy))
+  for dy = _NEIGHBOR_OFFSET_RANGE.min, _NEIGHBOR_OFFSET_RANGE.max do
+    for dx = _NEIGHBOR_OFFSET_RANGE.min, _NEIGHBOR_OFFSET_RANGE.max do
+      local translated_point = point + Vector2D:new(dx, dy)
       local alive = field:contains(translated_point)
       local central = dx == 0 and dy == 0
       if alive and not central then
